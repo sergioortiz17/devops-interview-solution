8335ab8191d25946264944badb26d2e29d20c995
sergioStori <sergio.ortiz.mendoza@mi.unc.edu.ar>
2025-02-16 00:01:40 -0300

    add cors config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -149,8 +149,33 @@ REST_FRAMEWORK = {
 # ##################################################################### #
 # ################### CORS              ############################### #
 # ##################################################################### #
+CORS_ALLOWED_ORIGINS = [
+    "http://3.82.218.240:3000",
+    "http://127.0.0.1:3000"
+]
 
-CORS_ALLOWED_ORIGINS = ["http://3.82.218.240:3000", "http://127.0.0.1:3000"]
+# Configuraciones adicionales NECESARIAS
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # ##################################################################### #
 # ################### TESTING           ############################### #
