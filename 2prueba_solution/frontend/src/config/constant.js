6200a900a7e2509cb9f66f6c7ef4adc85a8a4c1e
sergioStori <sergio.ortiz.mendoza@mi.unc.edu.ar>
2025-02-17 03:17:36 -0300

    Update constant.js

--- 2prueba_solution/frontend/src/config/constant.js
+++ 2prueba_solution/frontend/src/config/constant.js
@@ -1,7 +1,7 @@
 export const BASENAME = ''; // don't add '/' at end off BASENAME
 export const BASE_URL = '/app/dashboard/default';
 export const BASE_TITLE = ' | Craftech Example App ';
-export const API_SERVER = 'http://localhost:8000/api/';
+export const API_SERVER = 'http://52.90.91.118:8000/api/';
 
 export const CONFIG = {
     layout: 'vertical', // disable on free version
